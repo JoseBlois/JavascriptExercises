a9ea8d6b558297efc57090c3e95bcaa3239986f6
jose blois <joseblois3@gmail.com>
2020-10-26 19:42:37 -0300

    First array exercise 3.a done

--- js/app.js
+++ js/app.js
@@ -88,3 +88,13 @@ var secondHalf = sentence.substring(spacePoint+2);
 var finalSentence = start.toUpperCase() + firstHalf.toLowerCase() + secondStart.toUpperCase() + secondHalf.toLowerCase();
 console.log(finalSentence);
 
+//3.a)
+/*  Dado el siguiente array:
+    ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    mostrar por consola los meses 5 y 11 (utilizar console.log)*/
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(months[5],months[11]);
+
+//3.b)
+/*  Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
