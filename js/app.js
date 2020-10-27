a1f8613bc2014f9e46208276f7529524732b22aa
jose blois <joseblois3@gmail.com>
2020-10-27 12:16:05 -0300

    Done with exercise 6

--- js/app.js
+++ js/app.js
@@ -205,3 +205,72 @@ for(var p = 0; p < 10; p++){
     emptyArray[p] = p;
 }
 console.log(emptyArray);
+
+//6.a)
+/*  Crear una función suma que reciba dos valores numéricos y retorne el resultado.
+    Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha
+    variable en la consola del navegador.*/
+
+function suma(a,b){
+    return a + b;
+}
+var result = suma(5,5);
+console.log(result);
+
+//6.b)
+/*  A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros
+    no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar
+    el valor NaN como resultado*/
+
+function suma(a,b){
+    if( || typeof b !== 'number'){
+        alert('Theres an error with one of the parameters');
+        return NaN;
+    }
+    return a + b;
+}
+
+//6.c)
+/*  Crear una función validate integer que reciba un número como parámetro
+    y devuelva verdadero si es un número entero.*/
+
+function validateInteger(number){
+    truncedNumber = Math.trunc(number);
+    return !(number - truncedNumber > 0)
+}
+
+//6.d)
+/*  A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros.
+    En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero
+    (redondeado).*/
+
+function suma(a,b){
+    if( typeof a !== 'number' || typeof b !== 'number') {
+        alert('Theres an error with one of the parameters');
+        return NaN;
+    }
+    if ( !validateInteger(a) || !validateInteger(b) ){
+        alert('Error: One of the parameters is not an interger')
+        return Math.round(a+b);
+    }
+    return a + b;
+}
+
+//6.e)
+/*  Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función
+    suma probando que todo siga funcionando igual.*/
+
+function checkType(number){
+    return typeof number === 'number';
+}
+function suma(a,b){
+    if ( !checkType(a) && !checkType(b) ){
+        alert('Theres an error with one of the parameters');
+        return NaN;
+    }
+    if ( !validateInteger(a) || !validateInteger(b) ){
+        alert('Error: One of the parameters is not an interger')
+        return Math.round(a+b);
+    }
+    return a + b;
+}
